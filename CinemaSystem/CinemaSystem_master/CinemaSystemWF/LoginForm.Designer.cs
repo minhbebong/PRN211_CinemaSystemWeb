4a8bb870e38aa6b67140b24f63056cb93bb1513f
Nguyen Hoang Nhat <nhatnhhe161207@fpt.edu.vn>
2023-08-18 16:01:28 +0700

    Form Login , Main Form , ...

---
+++ CinemaSystem/CinemaSystem_master/CinemaSystemWF/LoginForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CinemaSystemWF
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            linkFogotPassword = new LinkLabel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(155, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(155, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(280, 90);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(212, 27);
+            txtEmail.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(280, 137);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(212, 27);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(280, 254);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(129, 39);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // linkFogotPassword
+            // 
+            linkFogotPassword.AutoSize = true;
+            linkFogotPassword.Location = new Point(432, 273);
+            linkFogotPassword.Name = "linkFogotPassword";
+            linkFogotPassword.Size = new Size(118, 20);
+            linkFogotPassword.TabIndex = 5;
+            linkFogotPassword.TabStop = true;
+            linkFogotPassword.Text = "Forgot Password";
+            linkFogotPassword.LinkClicked += linkFogotPassword_LinkClicked;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(linkFogotPassword);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private LinkLabel linkFogotPassword;
+    }
+}
---
+++ CinemaSystem/CinemaSystem_master/CinemaSystemWF/LoginForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CinemaSystemWF;
+using CinemeSystemWF.Requests;
+
+namespace CinemaSystemWF
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+
+            if (email.Length == 0 || password.Length == 0)
+            {
+                MessageBox.Show("Please fill all the fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var result = await AuthRequest.Instance.Login(email, password).ConfigureAwait(false);
+            if (result.Success)
+            {
+                Program.Token = result.Token;
+                this.Invoke(() =>
+                {
+                    this.Hide();
+                    MainForm form = new();
+                    form.Show();
+                });
+            }
+            else
+            {
+                MessageBox.Show(result.Message, "Error");
+            }
+        }
+
+        private void linkFogotPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("cmd", "");
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
