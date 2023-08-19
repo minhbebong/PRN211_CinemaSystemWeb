eac94b7d4eeeeb303a6e74bcff0769c3b1d0f8c0
minhndhe151002 <hichic375@gmail.com>
2023-08-19 14:43:28 +0700

    update Tickets, Setting User

--- CinemaSystem/CinemaSystem/CinemaSystemWebapp/Program.cs
+++ CinemaSystem/CinemaSystem/CinemaSystemWebapp/Program.cs
@@ -1,21 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
+// Configure the HTTP request pipeline
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseStatusCodePagesWithRedirects("/Error?code={0}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
