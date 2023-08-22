f217fcb5ba0af1e591175d6035c0c303898cbfde
truongbqhe160611 <buitruong091102@gmail.com>
2023-08-22 22:35:01 +0700

    admin,category,film

---
+++ Lab1/Lab1/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Lab1.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly PRN_DBContext db;
+        public OrdersController(PRN_DBContext db)
+        {
+            this.db = db;
+        }
+        public async Task<IActionResult> List()
+        {
+            if (HttpContext.Session.GetString("Account") != null)
+            {
+                var account = JsonSerializer.Deserialize<Account>(HttpContext.Session.GetString("Account"));
+                if (account.Role == 2)
+                {
+                    dynamic employees = null;
+                    //Lay gia tri cua tham so C.id tu url:Home/Index/?cid=x                 
+                    var orders = db.Orders.ToList();
+                    //dung ViewData de chuyen du lieu la danh sach cac city sang view
+                    ViewData["orders"] = orders;
+                    // dung phuong thuc View() de truyen du lieu la danh sach cac employee thong qua model
+                    return View(employees);
+                }
+                else
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+            }
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ PRN211_22_08/PRN211_22_08/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRN211_22_08.Models;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace PRN211_22_08.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly PRN_DBContext db;
+        public AccountController(PRN_DBContext db)
+        {
+            this.db = db;
+        }
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(Account acc)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            var account = await db.Accounts.Where(a => a.Email.Equals(acc.Email) && a.Password.Equals(acc.Password) && a.Role == 1).SingleOrDefaultAsync();
+            if (account == null)
+            {
+                TempData["msg"] = " Account not exist and Invalid role of employees";
+                return View();
+            }
+            // cap session (Phien) lam viec cho tai khoan nay
+            var JsonAccount = JsonSerializer.Serialize<Account>(account);
+            HttpContext.Session.SetString("Account", JsonAccount);
+            return RedirectToAction("List", "Orders");
+        }
+        public async Task<IActionResult> Logout()
+        {
+            if (HttpContext.Session.GetString("Account") != null)
+            {
+                //Huy session
+                HttpContext.Session.Remove("Account");
+            }
+            return RedirectToAction("Login", "Account");
+        }
+ 
+    }
+}
